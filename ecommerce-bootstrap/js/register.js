ace2999d81965244079c6d3289dd9fa6a25e1f1a
ethanrei <ethanreiarevalo@gmail.com>
2019-10-03 19:36:20 +0800

    register validation

--- ecommerce-bootstrap/js/register.js
+++ ecommerce-bootstrap/js/register.js
@@ -1,8 +1,1 @@
 
-function validation(){
-    var fname_input = document.getElementById("i_fname").value;
-    var fname_label = document.getElementById("l_fname").value;
-    if(fname_input === ""){
-        document.getElementById("l_fname").innerHTML = "Please fill out this form.";
-    }
-}
--- ecommerce-bootstrap/register.php
+++ ecommerce-bootstrap/register.php
@@ -66,21 +66,19 @@
             <div class="container bg-light">
 <!--*******************FORM REGISTER*************************-->
                 <h4 class="text-center">Register to OKAERI!</h4>
-                <form action="" method="post">
-                    <label id="l_fname" class="l_fname" for="fname"></label>
-                    <input id = "i_fname" class="form-control mr-sm-2 my-2 i_fname" type="text" placeholder="First Name">
+                <form action="" name="regForm" class="regform" method="post">
+                    <input id = "i_fname" name="fname" class="form-control mr-sm-2 my-2 i_fname" type="text" placeholder="First Name" minlength = "3" required>
                     <input class="form-control mr-sm-2 my-2" type="text" placeholder="Middle Name">
                     <input class="form-control mr-sm-2 my-2" type="text" placeholder="Last Name">
                     <input class="form-control mr-sm-2 my-2" type="text" placeholder="Age">
                     <input class="form-control mr-sm-2 my-2" type="text" placeholder="Contact Number">
                     <input class="form-control mr-sm-2 my-2" type="text" placeholder="Address">
-                    <input class="form-control mr-sm-2 my-2" type="text" placeholder="Email Adress">
-                    <input class="form-control mr-sm-2 my-2" type="text" placeholder="Verify Email Adress">
+                    <input class="form-control mr-sm-2 my-2" type="email" placeholder="Email Adress" required>
+                    <input class="form-control mr-sm-2 my-2" type="email" placeholder="Verify Email Adress" required>
                     <input class="form-control mr-sm-2 my-2" type="password" placeholder="Password">
                     <input class="form-control mr-sm-2 my-2" type="password" placeholder="Verify Password">
                     
-                    
-                    <button onClick="validation()" class="btn btn-outline-warning w-100 my-2 my-sm-0 text-dark" type="submit">Sign up!</button>
+                    <button name="submit" type="submit" value="Submit" class="btn btn-outline-warning w-100 my-2 my-sm-0 text-dark">Sign up!</button>
                 </form>
             </div>
         </div>

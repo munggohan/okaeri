8a73f5a686e4be09362b54015626845ea136f0be
ethanrei <ethanreiarevalo@gmail.com>
2019-10-11 19:15:34 +0800

    imageloading

--- ecommerce-bootstrap/login.php
+++ ecommerce-bootstrap/login.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OKAERI | Loading...</title>
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+    <img src="image/loading.png" alt="">
+</body>
+</html>
 <?php
     session_start();
     //connect to database
@@ -5,6 +21,7 @@
     $password = $_POST['password'];
     $notice = "Username or Password is Incorrect";
     $conn = mysqli_connect('localhost','root','','okaeridb');
+    
     //write query
     if(!$conn){
         echo("connection Failed! ". mysqli_connect_error());
